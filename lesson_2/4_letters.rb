65acd2c68987c039f1ea312a5786bd5298e3f2a7
jezman <dev.jezman@gmail.com>
2018-09-17 14:59:44 +0300

    Task 3.

--- lesson_2/4_letters.rb
+++
@@ -1,8 +0,0 @@
-vowels_array = %w[a e i o u y]
-vowels_hash = {}
-
-('a'..'z').each_with_index do |letter, index|
-  vowels_hash[letter] = index + 1 if vowels_array.include?(letter)
-end
-
-p vowels_hash
